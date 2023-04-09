ddca443faca554e6d095a64b284ddddfd3f9a935
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-04-09 23:07:39 +0530

    Add: progress bar

--- app/src/main/java/com/example/memeshareappkt/MainActivity.kt
+++ app/src/main/java/com/example/memeshareappkt/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.memeshareappkt
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -9,6 +10,10 @@ import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.example.memeshareappkt.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -22,8 +27,10 @@ class MainActivity : AppCompatActivity() {
         loadMeme()
     }
 
+    // function to make api call and populate the memeImageView
     private fun loadMeme() {
         // Instantiate the RequestQueue.
+        binding.progressBar.visibility = View.VISIBLE
         val queue = Volley.newRequestQueue(this)
         val url = "https://meme-api.com/gimme"
 
@@ -31,8 +38,31 @@ class MainActivity : AppCompatActivity() {
         val jsonObjectRequest = JsonObjectRequest(Request.Method.GET,url,null,
             { response ->
                 val url = response.getString("url")
-                Glide.with(this).load(url).into(binding.memeImageView)
+                Glide.with(this).load(url).listener(object: RequestListener<Drawable> {
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        binding.progressBar.visibility = View.GONE
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        binding.progressBar.visibility = View.GONE
+                        return false
+                    }
+
+                }).into(binding.memeImageView)
             }, {
+                binding.progressBar.visibility = View.GONE
                Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show()
         })
 
