78e2b606c47230eac9fbcc969c66167cdc854cfe
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-04-09 22:01:26 +0530

    Add: UI for meme share app

--- app/src/main/java/com/example/memeshareappkt/MainActivity.kt
+++ app/src/main/java/com/example/memeshareappkt/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.memeshareappkt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun shareMeme(view: View) {
+        print("Share")
+    }
+
+    fun nextMeme(view: View) {
+        print("Meme")
     }
 }
